1644664e5836f2eaa51ac2674111fa0938527ceb
Bys74 <bybou74300@hotmail.fr>
2025-06-27 10:35:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -81,3 +81,13 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > 10) {
+      header.classList.add("shrink");
+    } else {
+      header.classList.remove("shrink");
+    }
+  });
+
